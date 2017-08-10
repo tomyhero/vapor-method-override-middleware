df5cee219eda4a3d290277b56c22ae97953a9156
Tomohiro Teranishi <tomohiro.teranishi@gamil.com>
2017-08-10 17:36:40 +0900

    remove *.swp ...

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "vapor-method-override-middleware",
+    name: "MethodOverrideMiddleware",
     dependencies: [
       .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
     ]
